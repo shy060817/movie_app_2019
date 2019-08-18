47dd0ae2fdb882dbc0bae846eb9841a754152fe6
shy060817 <alex.wanjun@gmail.com>
2019-08-18 15:31:00 +0900

    Updates

---
+++ precache-manifest.ed7ceaf7a603e1e794ab502878dc3b5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4958556d75963245db7782c0e6e9aa",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "1dfa682799dde413b041",
+    "url": "/movie_app_2019/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "9b9d79ff65c89f1ae042",
+    "url": "/movie_app_2019/static/js/2.d488ae61.chunk.js"
+  },
+  {
+    "revision": "1dfa682799dde413b041",
+    "url": "/movie_app_2019/static/js/main.f342a67d.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
